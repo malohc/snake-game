83d67b2db5662c5a8f5ef4508c308b47d673a68e
Malik-hc <malikhcunningham6@gmail.com>
2025-09-17 14:03:57 -0400

    Created create_block fucntion

--- snake-game.py
+++ snake-game.py
@@ -3,6 +3,7 @@ import pygame
 COLUMNS = 60
 ROWS = 40
 BLOCKS = 10
+SNAKE_COLOR = (0, 255, 0)
 
 pygame.init()
 
@@ -14,6 +15,9 @@ clock = pygame.time.Clock()
 def gameLoop():
     game_over = False
 
+    x1 = COLUMNS/2
+    y1 = ROWS/2
+
     while not game_over:
         
         for event in pygame.event.get():
@@ -22,9 +26,17 @@ def gameLoop():
         
         screen.fill((0,0,0))
 
+        create_block([x1,y1], (SNAKE_COLOR))
+
         pygame.display.update()
 
         clock.tick(15)
+
+def create_block(position, color):
+    x = position[0] * BLOCKS
+    y = position[1] * BLOCKS
+
+    pygame.draw.rect(screen, color, (x, y, BLOCKS, BLOCKS))
 
 gameLoop()
 pygame.quit()
